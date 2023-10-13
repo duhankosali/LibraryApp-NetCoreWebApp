0a78ce2050699cb9be09c1acdcd189cd0bab79d7
duhankosali <duhanksali@gmail.com>
2023-10-13 21:59:57 +0300

    Core Layer

---
+++ LibraryApp.Core/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp.Core.UnitOfWorks
+{
+    public interface IUnitOfWork
+    {
+        Task CommitAsync(); // EF --> SaveChangeAsync
+        void Commit(); // EF --> SaveChange
+    }
+}
