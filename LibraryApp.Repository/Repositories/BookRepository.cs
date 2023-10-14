9d0524d409ed8af49fe8956bb7f5e93f43a3c935
duhankosali <duhanksali@gmail.com>
2023-10-14 18:32:21 +0300

    BookRepository, BookService etc.

---
+++ LibraryApp.Repository/Repositories/BookRepository.cs
@@ -0,0 +1,19 @@
+﻿using LibraryApp.Core.Entities;
+using LibraryApp.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp.Repository.Repositories
+{
+    public class BookRepository : GenericRepository<Book>, IBookRepository
+    {
+        public BookRepository(AppDbContext context) : base(context)
+        {
+
+        }
+    }
+}
---
+++ LibraryApp.Web/Controllers/BooksController.cs
@@ -0,0 +1,20 @@
+﻿using LibraryApp.Core.Entities;
+using LibraryApp.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryApp.Web.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly IBookService _service;
+        public BooksController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
